a75ded22ff0f094830c840d7a27359a0111f690f
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-20 17:50:34 +0200

    add contactlist and createcontact screens

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import SignUpScreen from './SignUp';
 import SignInScreen from './SignIn';
+import HomeScreenAfterLogin from './HomeScreenAfterLogin';
+import ContactList from './ContactList';
+import CreateContact from './CreateContact';
+
 
 const Stack = createStackNavigator();
 
@@ -17,6 +21,9 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="HomeAfterLogin" component={HomeScreenAfterLogin} />
+        <Stack.Screen name="ContactList" component={ContactList} />
+        <Stack.Screen name="CreateContact" component={CreateContact} />
       </Stack.Navigator>
     </NavigationContainer>
   );
