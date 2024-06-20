e177801c18785c48c3178ce6b243c0627d0cb8aa
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-20 13:14:49 +0200

    auth for last CRUD + middleware for json parsing

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 // MODULES
-
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('./config/passport');
-
 const app = express();
 
 // Middleware pour parser le JSON et gérer les CORS
 app.use(express.json());
 app.use(cors());
+
+// Middleware pour gérer les erreurs de parsing JSON
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      console.error('Invalid JSON or bad request', err);
+      return res.status(400).json({ error: 'Invalid JSON or bad request' });
+  }
+  next();
+});
 
 // Session middleware
 app.use(session({
@@ -45,9 +52,9 @@ app.use('/api/contacts', passport.authenticate('local'), contactsRoutes);
 app.use('/api/users', passport.authenticate('local'), usersRoutes);
 app.use('/api/neurodivergences', passport.authenticate('local'), neurodivergencesRoutes);
 app.use('/auth', authRoutes);
-app.use('/api/gifts', giftsRoutes);
-app.use('/api/gift_ideas', giftIdeasRoutes);
-app.use('/api/task-list', taskListRoutes);
+app.use('/api/gifts', passport.authenticate('local'), giftsRoutes);
+app.use('/api/gift_ideas', passport.authenticate('local'), giftIdeasRoutes);
+app.use('/api/task-list', passport.authenticate('local'), taskListRoutes);
 
 // Démarre le serveur
 const PORT = 3000;
