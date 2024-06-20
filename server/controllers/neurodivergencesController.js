e0c4440dc67d6c7ad0eb8dc50d854f6cb4636425
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-20 17:52:28 +0200

    fix syntax

--- server/controllers/neurodivergencesController.js
+++ server/controllers/neurodivergencesController.js
@@ -8,7 +8,7 @@ const handleServerError = (res, err) => {
 
 // Récupérer toutes les neurodivergences
 const getAllNeurodivergences = (req, res) => {
-  const query = 'SELECT * FROM neurodivergences';
+  const query = 'SELECT * FROM neurodivergence';
   
   db.query(query, (err, results) => {
     if (err) return handleServerError(res, err); // Utilisation de la fonction de gestion des erreurs
@@ -19,7 +19,7 @@ const getAllNeurodivergences = (req, res) => {
 // Créer une nouvelle neurodivergence
 const createNeurodivergence = (req, res) => {
   const { type } = req.body;
-  const query = 'INSERT INTO neurodivergences (type) VALUES (?)';
+  const query = 'INSERT INTO neurodivergence (type) VALUES (?)';
   
   db.query(query, [type], (err, results) => {
     if (err) return handleServerError(res, err); // Utilisation de la fonction de gestion des erreurs
@@ -31,7 +31,7 @@ const createNeurodivergence = (req, res) => {
 const updateNeurodivergence = (req, res) => {
   const id = req.params.id;
   const { type } = req.body;
-  const query = 'UPDATE neurodivergences SET type = ? WHERE id = ?';
+  const query = 'UPDATE neurodivergence SET type = ? WHERE id = ?';
   
   db.query(query, [type, id], (err, results) => {
     if (err) return handleServerError(res, err); // Utilisation de la fonction de gestion des erreurs
@@ -42,7 +42,7 @@ const updateNeurodivergence = (req, res) => {
 // Supprimer une neurodivergence
 const deleteNeurodivergence = (req, res) => {
   const id = req.params.id;
-  const query = 'DELETE FROM neurodivergences WHERE id = ?';
+  const query = 'DELETE FROM neurodivergence WHERE id = ?';
   
   db.query(query, [id], (err, results) => {
     if (err) return handleServerError(res, err); // Utilisation de la fonction de gestion des erreurs
