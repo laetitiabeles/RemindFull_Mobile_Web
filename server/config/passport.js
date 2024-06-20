66cbdfb615c4cad1c313846d8ba255e20d68e80f
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-20 13:15:03 +0200

    add debug logs for auth

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,54 +6,45 @@ const db = require('../db');
 
 passport.use(new LocalStrategy(
   {
-    usernameField: 'identifier', // email ou username
-    passwordField: 'password'
+      usernameField: 'identifier', // Renommé pour refléter qu'il peut être email ou username
+      passwordField: 'password'
   },
-  (identifier, password, done) => {
-    // Requête à la base de données pour trouver l'utilisateur par email ou username
-    const query = 'SELECT * FROM user WHERE email = ? OR username = ?';
-    db.query(query, [identifier, identifier], async (err, results) => {
-      if (err) {
-        return done(err);
-      }
-      if (!results || results.length === 0) {
-        return done(null, false, { message: 'User not found' });
-      }
-
-      const user = results[0];
-
-      // Vérifie le mot de passe haché
-      try {
-        if (await bcrypt.compare(password, user.password)) {
-          // Mot de passe correct
-          return done(null, user);
-        } else {
-          // Mot de passe incorrect
-          return done(null, false, { message: 'Incorrect password' });
-        }
-      } catch (error) {
-        return done(error);
-      }
-    });
+  function(identifier, password, done) {
+      console.log(`Attempting to authenticate user with identifier: ${identifier}`);
+      db.query('SELECT * FROM user WHERE username = ? OR email = ?', [identifier, identifier], function(err, results) {
+          if (err) {
+              console.error("Database error during authentication:", err);
+              return done(err);
+          }
+          if (!results.length) {
+              console.log(`No user found with identifier: ${identifier}`);
+              return done(null, false, { message: 'Incorrect username or email.' });
+          }
+          if (!bcrypt.compareSync(password, results[0].password)) {
+              console.log("Incorrect password provided for user:", identifier);
+              return done(null, false, { message: 'Incorrect password.' });
+          }
+          console.log(`User ${identifier} authenticated successfully`);
+          return done(null, results[0]);
+      });
   }
 ));
 
 // Sérialisation de l'utilisateur pour la session
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+  console.log(`Serializing user: ${user._id}`);
+  done(null, user.id);
 });
 
 // Désérialisation de l'utilisateur à partir de l'ID de session
 passport.deserializeUser((id, done) => {
-  const query = 'SELECT * FROM user WHERE _id = ?';
-  db.query(query, [id], (err, results) => {
-    if (err) {
-      return done(err);
-    }
-    if (!results || results.length === 0) {
-      return done(null, false);
-    }
-    done(null, results[0]);
+  console.log(`Deserializing user with ID: ${id}`);
+  db.query('SELECT * FROM user WHERE _id = ?', [id], function(err, results) {
+      if (err) {
+          console.error("Error deserializing user:", err);
+          return done(err);
+      }
+      done(null, results[0]);
   });
 });
 
