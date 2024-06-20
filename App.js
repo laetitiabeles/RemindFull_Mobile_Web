6a4a0930f5cae96ae7b45fb53a5e8488b10452b5
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-20 10:27:03 +0200

    Change the navigation for Homescreen

--- App.js
+++ App.js
@@ -2,37 +2,24 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import CreateContact from './CreateContact';
-import ContactList from './ContactList';
-import UpdateContact from './UpdateContact';
+import HomeScreen from './HomeScreen';
+import SignUpScreen from './SignUp';
+import SignInScreen from './SignIn';
 
 const Stack = createStackNavigator();
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome to the Contact App</Text>
-      <Button
-        title="Create Contact"
-        onPress={() => navigation.navigate('CreateContact')}
-      />
-      <Button
-        title="Contact List"
-        onPress={() => navigation.navigate('ContactList')}
-      />
-    </View>
-  );
-}
+// Main App
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name="CreateContact" component={CreateContact} options={{ title: 'Create Contact' }} />
-        <Stack.Screen name="ContactList" component={ContactList} options={{ title: 'Contact List' }} />
-        <Stack.Screen name="UpdateContact" component={UpdateContact} options={{ title: 'Update Contact' }} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
