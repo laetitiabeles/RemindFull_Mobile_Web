ea7f095fc9c24821bf529e20b1c521ee7396018f
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-25 17:21:39 +0200

    add search bar and filtering contact

--- ContactList.js
+++ ContactList.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, Button, Alert } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button, Alert, TextInput } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import axios from 'axios';
+import BASE_URL from './config';
 import { format } from 'date-fns';
 
 const ContactList = ({ navigation }) => {
   const [contacts, setContacts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredContacts, setFilteredContacts] = useState([]);
 
   const fetchContacts = () => {
-    axios.get('http://10.0.2.2:3000/api/contacts')
+    axios.get(`${BASE_URL}/api/contacts`)
       .then(response => {
         console.log('Data received:', response.data);
         setContacts(response.data);
+        setFilteredContacts(response.data);
       })
       .catch(error => {
         console.error('Error fetching contacts:', error);
@@ -26,12 +30,28 @@ const ContactList = ({ navigation }) => {
 
   useFocusEffect(
     React.useCallback(() => {
-        fetchContacts();
+      fetchContacts();
     }, [])
-);
+  );
 
   const formattedDate = (date) => {
     return format(new Date(date), 'dd-MM-yyyy');
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    console.log('Search query:', query); // Log search query
+    if (query === '') {
+      setFilteredContacts(contacts);
+    } else {
+      const filtered = contacts.filter(contact =>
+        contact.first_name.toLowerCase().includes(query.toLowerCase()) ||
+        contact.last_name.toLowerCase().includes(query.toLowerCase()) ||
+        (contact.neurodivergence?.toLowerCase() ?? '').includes(query.toLowerCase())
+      );
+      console.log('Filtered contacts:', filtered); // Log filtered contacts
+      setFilteredContacts(filtered);
+    }
   };
 
   const handleDelete = (contactId) => {
@@ -44,8 +64,8 @@ const ContactList = ({ navigation }) => {
           onPress: () => console.log("Delete canceled"),
           style: "cancel"
         },
-        { 
-          text: "OK", 
+        {
+          text: "OK",
           onPress: () => deleteContact(contactId)
         }
       ]
@@ -53,10 +73,12 @@ const ContactList = ({ navigation }) => {
   };
 
   const deleteContact = (contactId) => {
-    axios.delete(`http://10.0.2.2:3000/api/contacts/${contactId}`)
+    axios.delete(`http://${BASE_URL}/api/contacts/${contactId}`)
       .then(response => {
         Alert.alert('Success', 'Contact deleted successfully');
-        setContacts(contacts.filter(contact => contact._id !== contactId));
+        const updatedContacts = contacts.filter(contact => contact._id !== contactId);
+        setContacts(updatedContacts);
+        setFilteredContacts(updatedContacts);
       })
       .catch(error => {
         console.error('Error deleting contact:', error);
@@ -66,38 +88,51 @@ const ContactList = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Contact List</Text>
-      <Button
-        title="Create Contact"
-        onPress={() => navigation.navigate('CreateContact')} // Assurez-vous que 'CreateContact' est le bon nom de la route
-        color="#007BFF" // Couleur du bouton, ici bleu
+      <Text style={styles.title}>Contacts</Text>
+      <TextInput
+        style={styles.searchBar}
+        placeholder="Rechercher un contact ou une neurodivergence"
+        value={searchQuery}
+        onChangeText={handleSearch}
       />
-      <FlatList
-        data={contacts}
-        keyExtractor={(item) => item._id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.contactItem}>
-            <Text style={styles.contactText}>Name: {item.first_name} {item.last_name}</Text>
-            <Text style={styles.contactText}>Email: {item.email}</Text>
-            <Text style={styles.contactText}>Phone Number: {item.phone_number}</Text>
-            <Text style={styles.contactText}>Birthday: {formattedDate(item.birthday)}</Text>
-            <Text style={styles.contactText}>Neurodivergence: {item.neurodivergence}</Text>
-            <Text style={styles.contactText}>Last contact: {formattedDate(item.last_contact)}</Text>
-            <Button
-              title="Delete"
-              onPress={() => handleDelete(item._id)}
-              color="#ff6347"
-            />
-            <Button
-              title="Update"
-              onPress={() => navigation.navigate('UpdateContact', { contact: item })}
-            />
-          </View>
-        )}
-      />
+      {filteredContacts.length === 0 ? (
+        <Text style={styles.noContactsText}>Aucun contact</Text>
+      ) : (
+        <View>
+          <Button
+            title="Créer un contact"
+            onPress={() => navigation.navigate('CreateContact')}
+            color="#007BFF"
+          />
+          <FlatList
+            data={filteredContacts}
+            keyExtractor={(item) => item._id.toString()}
+            renderItem={({ item }) => (
+              <View style={styles.contactItem}>
+                <Text style={styles.contactText}>Nom: {item.first_name} {item.last_name}</Text>
+                <Text style={styles.contactText}>Email: {item.email}</Text>
+                <Text style={styles.contactText}>Téléphone: {item.phone_number}</Text>
+                <Text style={styles.contactText}>Anniversaire: {formattedDate(item.birthday)}</Text>
+                <Text style={styles.contactText}>Neurodivergence: {item.neurodivergence}</Text>
+                <Text style={styles.contactText}>Dernier contact: {formattedDate(item.last_contact)}</Text>
+                <Button
+                  title="Supprimer"
+                  onPress={() => handleDelete(item._id)}
+                  color="#ff6347"
+                />
+                <Button
+                  title="Modifier"
+                  onPress={() => navigation.navigate('UpdateContact', { contact: item })}
+                />
+              </View>
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -108,6 +143,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+  },
+  searchBar: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 8,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  noContactsText: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 20,
   },
   contactItem: {
     padding: 10,
