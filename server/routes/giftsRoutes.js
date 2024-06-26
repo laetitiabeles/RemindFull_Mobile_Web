76b1525ffc322de9e741424bad8bbb49ff5b3ce0
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-26 16:17:17 +0200

    add logs

--- server/routes/giftsRoutes.js
+++ server/routes/giftsRoutes.js
@@ -3,10 +3,35 @@ const router = express.Router();
 const giftsController = require('../controllers/giftsController');
 
 // Routes pour les gifts
-router.get('/', giftsController.getAllGifts);
-router.get('/:giftId', giftsController.getGiftById);
-router.post('/', giftsController.createGift);
-router.put('/:giftId', giftsController.updateGift);
-router.delete('/:giftId', giftsController.deleteGift);
+router.get('/', (req, res, next) => {
+  console.log("Received request to get all gifts");
+  next();
+}, giftsController.getAllGifts);
+
+router.get('/:giftId', (req, res, next) => {
+  console.log(`Received request to get gift with ID: ${req.params.giftId}`);
+  next();
+}, giftsController.getGiftById);
+
+router.get('/contact/:contactId', (req, res, next) => {
+  console.log(`Received request to get gifts for contact ID: ${req.params.contactId}`);
+  next();
+}, giftsController.getGiftsByContactId); // Ajouter cette route pour récupérer les cadeaux par contact
+
+router.post('/', (req, res, next) => {
+  console.log("Received request to create a new gift with data:", req.body);
+  next();
+}, giftsController.createGift);
+
+router.put('/:giftId', (req, res, next) => {
+  console.log(`Received request to update gift with ID: ${req.params.giftId}`);
+  console.log("Update data:", req.body);
+  next();
+}, giftsController.updateGift);
+
+router.delete('/:giftId', (req, res, next) => {
+  console.log(`Received request to delete gift with ID: ${req.params.giftId}`);
+  next();
+}, giftsController.deleteGift);
 
 module.exports = router;
