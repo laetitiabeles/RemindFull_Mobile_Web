a1894f1f3282be87b2a0486978289680b50373f6
laetitiabeles <laetitiabeles9@gmail.com>
2024-06-26 16:17:02 +0200

    add get gift by contactid route

--- server/routes/giftIdeasRoutes.js
+++ server/routes/giftIdeasRoutes.js
@@ -5,6 +5,10 @@ const giftIdeasController = require('../controllers/giftIdeasController');
 // Routes pour les gift ideas
 router.get('/', giftIdeasController.getAllGiftIdeas);
 router.get('/:ideaId', giftIdeasController.getGiftIdeaById);
+router.get('/contact/:contactId', (req, res, next) => {
+    console.log(`Received request to get gift ideas for contact ID: ${req.params.contactId}`);
+    next();
+  }, giftIdeasController.getGiftIdeasByContactId);
 router.post('/', giftIdeasController.createGiftIdea);
 router.put('/:ideaId', giftIdeasController.updateGiftIdea);
 router.delete('/:ideaId', giftIdeasController.deleteGiftIdea);
