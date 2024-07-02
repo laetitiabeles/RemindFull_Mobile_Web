4bab61e165d97ca4eb07fea27e624da7dfdfd2a3
laetitiabeles <laetitiabeles9@gmail.com>
2024-07-02 17:44:46 +0200

    add days before birthday

--- HomeScreenAfterLogin.js
+++ HomeScreenAfterLogin.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Button, View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, View, Text, FlatList, StyleSheet, TouchableOpacity, Pressable} from 'react-native';
 import Logout from './assets/logout.svg';
 import Moon from './assets/moon.svg';
 import CheckBox from '@react-native-community/checkbox';
 import RFLogo from './assets/RFLogo.svg';
 import axios from 'axios';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
-import { format, differenceInDays } from 'date-fns';
+import { format, differenceInDays, parseISO, isToday, startOfDay } from 'date-fns';
 import LinearGradient from 'react-native-linear-gradient';
 import * as Animatable from 'react-native-animatable';
 
@@ -77,13 +77,10 @@ const HomeScreenAfterLogin = () => {
             ]}
             onPress={() => handleButtonPress(taskButtonRef, 'Tasks')}
           >
-            <Text style={styles.segmentText}>📝 Tasks</Text>
+            <Text style={selectedSegment === 'Tasks' ? styles.selectedSegmentText : styles.segmentText}>📝 Tasks</Text>
           </TouchableOpacity>
         </Animatable.View>
       </LinearGradient>
-      <View style={styles.roundedShape}>
-        <Text></Text>
-      </View>
       <Animatable.View ref={segmentRef} style={{ flex: 1 }}>
         {selectedSegment === 'Contacts' ? <ContactsTab /> : <TasksTab />}
       </Animatable.View>
@@ -93,19 +90,49 @@ const HomeScreenAfterLogin = () => {
 
 const ContactsTab = () => {
   const [contacts, setContacts] = useState([]);
+  const [upcomingBirthdays, setUpcomingBirthdays] = useState([]);
   const navigation = useNavigation();
   const isFocused = useIsFocused();
+
+  const calculateDaysUntilBirthday = (birthday) => {
+    const today = startOfDay(new Date());
+    const nextBirthday = new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate());
+
+    if (isToday(nextBirthday)) {
+      return 0;
+    }
+
+    if (today > nextBirthday) {
+      nextBirthday.setFullYear(today.getFullYear() + 1);
+    }
+
+    return differenceInDays(nextBirthday, today);
+  };
 
   const fetchContacts = async () => {
     try {
       const response = await axios.get(`http://10.0.2.2:3000/api/contacts`);
-      const today = new Date();
+      const today = startOfDay(new Date());
       const filteredContacts = response.data.filter(contact => {
-        const lastContactDate = new Date(contact.last_contact);
+        const lastContactDate = startOfDay(new Date(contact.last_contact));
         const daysDifference = differenceInDays(today, lastContactDate);
         return daysDifference > 15;
       });
+      const upcomingBdays = response.data.filter(contact => {
+        const birthday = parseISO(contact.birthday);
+        const daysUntilBirthday = calculateDaysUntilBirthday(birthday);
+        return daysUntilBirthday >= 0 && daysUntilBirthday <= 7;
+      }).sort((a, b) => {
+        const birthdayA = parseISO(a.birthday);
+        const birthdayB = parseISO(b.birthday);
+        const nextBirthdayA = new Date(today.getFullYear(), birthdayA.getMonth(), birthdayA.getDate());
+        const nextBirthdayB = new Date(today.getFullYear(), birthdayB.getMonth(), birthdayB.getDate());
+        if (nextBirthdayA < today) nextBirthdayA.setFullYear(today.getFullYear() + 1);
+        if (nextBirthdayB < today) nextBirthdayB.setFullYear(today.getFullYear() + 1);
+        return nextBirthdayA - nextBirthdayB;
+      });
       setContacts(filteredContacts);
+      setUpcomingBirthdays(upcomingBdays);
     } catch (error) {
       console.error('Failed to fetch contacts:', error);
     }
@@ -129,20 +156,66 @@ const ContactsTab = () => {
     </TouchableOpacity>
   );
 
+  const renderBirthdayItem = ({ item }) => {
+    const birthday = parseISO(item.birthday);
+    const today = startOfDay(new Date());
+    const nextBirthday = new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate());
+
+    if (today > nextBirthday) {
+      nextBirthday.setFullYear(today.getFullYear() + 1);
+    }
+
+    const daysUntilBirthday = differenceInDays(nextBirthday, today);
+
+    return (
+      <TouchableOpacity
+        style={styles.contactItem}
+        onPress={() => navigation.navigate('ContactDetails', { contactId: item._id })}
+      >
+        <View style={styles.contactContent}>
+          <RFLogo style={styles.RFLogo} width={100} height={100} />
+          <Text style={styles.contactText}>{item.first_name} {item.last_name}</Text>
+          <Text style={styles.daysUntilText}>
+            {daysUntilBirthday === 0 ? "C'est son anniversaire" : `Dans ${daysUntilBirthday} jour${daysUntilBirthday > 1 ? 's' : ''}`}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.contactHeader}>Ça fait longtemps 💡</Text>
-      {contacts.length === 0 ? (
-        <Text style={styles.noContactsText}>Aucun contact à recontacter</Text>
-      ) : (
-        <FlatList
-          data={contacts}
-          renderItem={renderContactItem}
-          keyExtractor={(item) => item._id.toString()}
-          horizontal={true}
-          contentContainerStyle={styles.horizontalListContainer}
-        />
-      )}
+      <View style={styles.contactsContainer}>
+        {contacts.length === 0 ? (
+          <Text style={styles.noContactsText}>Aucun contact à recontacter</Text>
+        ) : (
+          <FlatList
+            data={contacts}
+            renderItem={renderContactItem}
+            keyExtractor={(item) => item._id.toString()}
+            horizontal={true}
+            contentContainerStyle={styles.horizontalListContainer}
+          />
+        )}
+      </View>
+      <TouchableOpacity style={styles.contactButton} onPress={() => navigation.navigate('ContactList')}>
+        <Text style={styles.contactButtonText}>Voir tous les contacts</Text>
+      </TouchableOpacity>
+      <Text style={styles.contactHeader}>C'est bientôt leur anniversaire 🎂</Text>
+      <View style={styles.contactsContainer}>
+        {upcomingBirthdays.length === 0 ? (
+          <Text style={styles.noContactsText}>Aucun anniversaire à venir</Text>
+        ) : (
+          <FlatList
+            data={upcomingBirthdays}
+            renderItem={renderBirthdayItem}
+            keyExtractor={(item) => item._id.toString()}
+            horizontal={true}
+            contentContainerStyle={styles.horizontalListContainer}
+          />
+        )}
+      </View>
     </View>
   );
 };
@@ -195,14 +268,17 @@ const TasksTab = () => {
         onPress={() => navigation.navigate('TaskDetails', { taskId: item._id })}
       >
         <CheckBox
+          style={styles.checkbox}
+          boxType='circle'
           value={false}
           onValueChange={() => handleDelete(item._id)}
+          tintColors={{ true: '#031D44', false: '#031D44' }}
         />
         <View style={styles.taskTextContainer}>
           <Text style={styles.taskTitle}>{item.task}</Text>
-          <Text>{item.task_description}</Text>
-          <Text>Priorité: {item.priority}</Text>
-          <Text>Échéance: {format(new Date(item.due_date), 'dd-MM-yyyy')}</Text>
+          <Text style={styles.taskText}>{item.task_description}</Text>
+          <Text style={styles.taskText}>Priorité: {item.priority}</Text>
+          <Text style={styles.taskText}>Échéance: {format(new Date(item.due_date), 'dd-MM-yyyy')}</Text>
         </View>
       </TouchableOpacity>
     );
@@ -221,10 +297,9 @@ const TasksTab = () => {
         />
       )}
       <View style={styles.buttonContainer}>
-        <Button
-          title="View all tasks"
-          onPress={() => navigation.navigate('TaskList')}
-        />
+      <TouchableOpacity style={styles.taskButton} onPress={() => navigation.navigate('TaskList')}>
+        <Text style={styles.taskButtonText}>Voir toutes les tâches</Text>
+      </TouchableOpacity>
       </View>
     </View>
   );
@@ -234,7 +309,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f9f9ea',
+    backgroundColor: 'white',
   },
   iconContainer: {
     flexDirection: 'row',
@@ -242,6 +317,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 20,
     paddingVertical: 10,
+    backgroundColor: '#fff',
+  },
+  logoutContainer: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+  },
+  moonContainer: {
+    alignSelf: 'flex-end',
     backgroundColor: '#fff',
   },
   header: {
@@ -278,57 +361,20 @@ const styles = StyleSheet.create({
     fontFamily: 'Inter-SemiBold',
     color: '#031D44',
   },
-  roundedShape: {
-    backgroundColor: '#f9f9ea',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    marginTop: 20, // Adjust as needed to overlap the segment control
-    elevation: 15,
-    height: 40,
+  selectedSegmentText: {
+    color: 'white',
   },
-  logoutContainer: {
-    alignSelf: 'flex-start',
-    backgroundColor: '#fff',
-  },
-  moonContainer: {
-    alignSelf: 'flex-end',
-    backgroundColor: '#fff',
+  contactsContainer: {
+    backgroundColor: '#031D44',
+    borderRadius: 20,
+    width: '100%',
   },
   contactHeader: {
     fontSize: 18,
-    marginBottom: 10,
+    marginBottom: 20,
     width: '100%',
     fontFamily: 'Inter-SemiBold',
     color: '#031D44',
-  },
-  taskHeader: {
-    fontSize: 16,
-    marginBottom: 10,
-    width: '100%',
-  },
-  taskItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    width: '100%',
-  },
-  taskTextContainer: {
-    flex: 1,
-    marginLeft: 10,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-  },
-  taskTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  noTasksText: {
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize: 16,
-    color: '#666',
   },
   listContainer: {
     width: '100%',
@@ -344,10 +390,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 15,
     borderRadius: 25,
-    marginRight: 25,
-    backgroundColor: 'white',
-    height: 150, // Adjust as needed
-    width: 150,  // Adjust as needed
+    marginRight: 12,
+    marginLeft: 18,
+    height: 150,
+    width: 140,
     elevation: 0,
   },
   contactContent: {
@@ -359,16 +405,90 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: 'center',
     fontFamily: 'Inter-SemiBold',
-    color: '#031D44',
+    color: 'white',
+  },
+  daysUntilText: {
+    fontSize: 12,
+    textAlign: 'center',
+    fontFamily: 'Inter-Regular',
+    color: 'white',
   },
   RFLogo: {
-    marginBottom: 10, // Add some space between the logo and the text
+    marginBottom: 10,
   },
   noContactsText: {
     textAlign: 'center',
     marginTop: 20,
     fontSize: 16,
     color: '#666',
+  },
+  contactButton: {
+    backgroundColor: '#031D44',
+    borderRadius: 20,
+    padding: 15,
+    marginVertical: 10,
+    marginBottom: 30,
+    width: '100%',
+  },
+  contactButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'Inter-SemiBold',
+    textAlign: 'center',
+  },
+  taskHeader: {
+    fontFamily: 'Inter-SemiBold',
+    fontSize: 16,
+    marginBottom: 10,
+    width: '100%',
+  },
+  taskItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#F5F5F5',
+    width: '100%',
+  },
+  taskTextContainer: {
+    flex: 1,
+    marginLeft: 10,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  taskTitle: {
+    fontFamily: 'Inter-SemiBold',
+    color: '#031D44',
+    fontSize: 18,
+    
+  },
+  taskText: {
+    fontFamily: 'Inter-Regular',
+    color: '#031D44',
+    fontSize: 14,
+  },
+  noTasksText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+    color: '#666',
+  },
+  checkbox: {
+    width: 30, // Ajuster la taille du CheckBox
+    height: 30, // Ajuster la taille du CheckBox
+  },
+  taskButton: {
+    backgroundColor: '#031D44',
+    borderRadius: 20,
+    padding: 15,
+    marginVertical: 10,
+    width: '100%',
+  },
+  taskButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'Inter-SemiBold',
+    textAlign: 'center',
   },
   buttonContainer: {
     marginVertical: 10,
