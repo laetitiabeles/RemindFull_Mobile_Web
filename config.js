a88f90edc3ef0769874294b4f85193143c25f844
laetitiabeles <laetitiabeles9@gmail.com>
2024-07-05 19:36:18 +0200

    fix IP adress for iOS

--- config.js
+++ config.js
@@ -1,7 +1,25 @@
 // config.js
 import { Platform } from 'react-native';
+import axios from 'axios';
 
-const BASE_URL = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+const BASE_URL = Platform.OS === 'ios' ? 'http://10.8.0.83:3000' : 'http://10.0.2.2:3000';
 console.log(`BASE_URL: ${BASE_URL}`);
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+      if (error.response) {
+        console.log('Response error:', error.response.data);
+        console.log('Status:', error.response.status);
+        console.log('Headers:', error.response.headers);
+      } else if (error.request) {
+        console.log('Request error:', error.request);
+      } else {
+        console.log('Error message:', error.message);
+      }
+      return Promise.reject(error);
+    }
+  );
+  
 
 export default BASE_URL;
